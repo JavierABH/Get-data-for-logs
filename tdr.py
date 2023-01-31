6bd49e82478b1805982cd01c98be01d99efc83a4
JavierABH <javierfchernandez10@gmail.com>
2023-01-31 14:43:32 -0600

    uploaded

--- tdr.py
+++ tdr.py
@@ -7,6 +7,15 @@ import pandas as pd
 
 path = "data/sample/test.csv"
 
-# Leer archivo original en un dataframe de pandas
+# Leer el primer dataframe
 df1 = pd.read_csv( path, nrows=3, header= 0)
 df1 = df1.iloc[:, 5:]
+
+# Leer el segundo dataframe
+df2 = pd.read_csv( path, header= 4)
+df2 = df2[['SN', 'Date', 'CycleTime', 'Status', 'FirstFail']]
+# Eliminar s de valores"
+df2['CycleTime'] = df2['CycleTime'].str.replace('s', '')
+
+# Añadir una columna "Measurement" al segundo dataframe
+df2['Measurement'] = 0.0
