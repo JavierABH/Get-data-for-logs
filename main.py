7ce4c717b43e233c9f97a939634e385da0577bfb
JavierABH <javierfchernandez10@gmail.com>
2023-02-01 15:01:54 -0600

    uploaded csv modify function (works)

--- main.py
+++ main.py
@@ -4,21 +4,29 @@ import glob
 import pandas as pd
 import numpy as np
 
+path = "data/sample/input.csv"
+
 def main():
     logs_path = "data\Results"
-    for path_file in glob.iglob(f'{logs_path}/**', recursive = True):
-        if os.path.isfile(path_file):
-            # Abrir el archivo csv para lectura
+    # Busca los archivos del directorio
+    # for path_file in glob.iglob(f'{logs_path}/**', recursive = True):
+    #     # Checa si es un archivo y que no sea GRR
+    #     if os.path.isfile(path_file) and "GRR" not in path_file:
+    #        change_csv(path_file)
+
+def change_csv(path_file):
+    # Abrir el archivo csv para lectura
             with open(path_file, 'r') as logfile:
                 reader = csv.reader(logfile)
                 # Leer todas las filas en una lista
                 rows = [row for row in reader]
+                csv_header1 = rows[0][6:]
 
-                # Buscar la fila con la palabra "SN" en la fila 4 o 5
+            # Buscar la fila con la palabra "SN" en la fila 4 o 5
                 for i, row in enumerate(rows):
                     if i in [3, 4] and "SN" in row:
-                        # Agregar 150 "," a la fila encontrada
-                        row += [""] * 150
+                        # Copia el nombre de las pruebas en la fila para el dataframe
+                        row += csv_header1
                         break
 
             # Abrir el archivo csv para escritura
