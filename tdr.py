ff1d185cb788db81c034e7379f2ef9ef8f319070
JavierABH <javierfchernandez10@gmail.com>
2023-02-07 11:41:31 -0600

    add partnumber function works

--- tdr.py
+++ tdr.py
@@ -4,11 +4,28 @@ from the generated csv file.
 """
 
 import pandas as pd
+import clr
+import configparser
+
+#local modules
+config = configparser.ConfigParser()
+config.read('config\settings.ini')
+
+newtonsoftjson_path = config["Paths"]["Newtonsoft.Json.Dll"]
+wsconnector_path = config["Paths"]["WSConnector.Dll"]
+
+clr.AddReference(newtonsoftjson_path)
+clr.AddReference(wsconnector_path)
+
+from WSConnector import Connector
+
+connector = Connector()
 
 class Csv_tdr:
     def __init__(self, path) -> None:
         self.path = path
         self.df_temp = None
+
         self.list_SerialNumber = None
         self.list_Datetime = None
         self.list_Date = None
@@ -17,6 +34,7 @@ class Csv_tdr:
         self.list_Status = None
         self.list_FirstFail = None
         self.list_Measurement = None
+
         self.df_temp = None
         self.set_tempdataframe()
 
@@ -29,7 +47,7 @@ class Csv_tdr:
         self.list_FirstFail = self.df_temp["FirstFail"]
         self.list_Date = self.list_Datetime.str.split(" ").str[0]
 
-        self.df_temp["Measurement"] = float("NaN")
+        self.df_temp["Measurement"] = ""
         for i, row in self.df_temp.iterrows():
             if row["Status"] == "Failed":
                 measurement = row["FirstFail"]
@@ -37,8 +55,13 @@ class Csv_tdr:
                 measurement_column = self.df_temp.columns.get_loc(measurement)
                 # write the value of the measurement in the new column "Measurement"
                 self.df_temp.at[i, "Measurement"] = self.df_temp.iloc[i, measurement_column]
-        self.list_Measurement = self.df_temp["Measurement"]
 
+            serial = row["SN"]
+            resp, serial_partnumber = connector.CIMP_PartNumberRef(serial,1, serial_partnumber)
+            self.df_temp.at[i, "PartNumber"] = serial_partnumber        
+
+        self.list_Measurement = self.df_temp["Measurement"]
+        self.list_PartNumber = self.df_temp["PartNumber"]
         
 
     
